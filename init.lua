50dca74477e7b9c7fb44b12927818f5f4a436357
Marco <46133335+Marco4413@users.noreply.github.com>
2023-11-11 01:29:17 +0100

    Removed duplicate code from ResetConfig

--- init.lua
+++ init.lua
@@ -43,18 +43,10 @@ local BetterSleeves = {
 function BetterSleeves:ResetConfig()
     self.autoRoll = true
     self.rollDownDelay = 1.0
-    self.rollDownItemBlacklist = {
-        ["outfit_01__q305__hazmat_"] = true,
-        ["outfit_01__q303__diving_suit_"] = true,
-        ["outfit_01__q303__diving_suit_ow_helmet_"] = true,
-        ["outfit_02_sq029_police_suit_"] = true,
-        ["outfit_02_sq030_diving_suit_"] = true,
-        ["outfit_02_q101_recovery_bandage_"] = true,
-    }
-    self.rollDownWeaponBlacklist = {
-        ["mantis_blade"] = true,
-        ["projectile_launcher"] = true,
-    }
+    self.rollDownItemBlacklist = {}
+    self.rollDownWeaponBlacklist = {}
+    -- Add all blacklist entries
+    self:MigrateConfigFromVersion(nil)
 end
 
 function BetterSleeves:SaveConfig()
